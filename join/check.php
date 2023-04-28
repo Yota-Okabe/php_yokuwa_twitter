5702896aa989bdbe27a0c86a1acd595eb37c104c
Yota Okabe <93124377+Yota-Okabe@users.noreply.github.com>
2023-04-28 17:13:10 +0900

    会員登録　見た目を作成

---
+++ join/check.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
+    <title>つぶやき掲示板</title>
+</head>
+<body>
+<div id="wrap">
+    <div id="head">
+        <h1>会員登録</h1>
+    </div>
+    <div id="content">
+    <p>次のフォームに必要事項を入力ください</p>
+    <form action="" method="post" enctype="multipart/form-data">
+        <dl>
+            <dt>ニックネーム</dt>
+                <dd></dd>
+            <dt>メールアドレス</dt>
+                <dd></dd>
+            <dt>パスワード</dt>
+                <dd>*表示されません*</dd>
+            <dt>写真など</dt>
+                <dd></dd>
+        </dl>
+        <div>
+            <a href="index.php?action=rewrite">&laquo;&nbsp修正する</a>
+            |
+            <input type="submit" value="登録する">
+        </div>
+    </form>
+    </div>
+</div>
+</body>
+</html>
