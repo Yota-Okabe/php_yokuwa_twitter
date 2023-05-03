66ce8d845f3bd418c8f84cae6db011fcb37cf4a8
Yota Okabe <93124377+Yota-Okabe@users.noreply.github.com>
2023-05-03 18:32:21 +0900

    つぶやきに返信新機能追加

--- index.php
+++ index.php
@@ -15,10 +15,11 @@ if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()) {
 
 if (!empty($_POST)) {
     if ($_POST['message'] != '') {
-        $message = $db->prepare('INSERT INTO posts SET member_id=?, message=?,created=NOW()');
+        $message = $db->prepare('INSERT INTO posts SET member_id=?, message=?, reply_post_id=?, created=NOW()');
         $message->execute(array(
             $member['id'],
-            $_POST['message']
+            $_POST['message'],
+            $_POST['reply_post_id']
         ));
         header('Location: index.php');
         exit();
@@ -27,6 +28,12 @@ if (!empty($_POST)) {
 
 $posts = $db->query('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE m.id=p.member_id ORDER BY p.created DESC');
 
+if (isset($_REQUEST['res'])) {
+    $response = $db->prepare('SELECT m.name, p.* FROM members m, posts p WHERE m.id=p.member_id AND p.id=? ORDER BY p.created DESC');
+    $response->execute(array($_REQUEST['res']));
+    $table = $response->fetch();
+    $message = '@' . $table['name'] . ' ' . $table['message'];
+}
 ?>
 
 <!DOCTYPE html>
@@ -50,7 +57,10 @@ $posts = $db->query('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE
                     <?php echo htmlspecialchars($member['name'], ENT_QUOTES) ?>さん、つぶやきをどうぞ
                 </dt>
                 <dd>
-                    <textarea name="message" cols="50" rows="5"></textarea>
+                    <textarea name="message" cols="50" rows="5">
+                        <?php echo htmlspecialchars($message, ENT_QUOTES); ?>
+                    </textarea>
+                    <input type="hidden" name="reply_post_id" value="<?php echo htmlspecialchars($_REQUEST['res'],ENT_QUOTES); ?>">
                 </dd>
             </dl>
             <div>
@@ -61,15 +71,16 @@ $posts = $db->query('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE
 <?php
 foreach ($posts as $post):
 ?>
-
         <div class="msg">
-        
             <img src="./join/member_picture/<?php echo htmlspecialchars($post['picture'], ENT_QUOTES) ?>" alt="<?php echo htmlspecialchars($post['name'], ENT_QUOTES) ?>" width="48" height="48">
-            <p><?php echo htmlspecialchars($post['message'], ENT_QUOTES) ?><span class="name">（<?php echo htmlspecialchars($post['name'], ENT_QUOTES) ?>）</span></p>
+            <p><?php echo htmlspecialchars($post['message'], ENT_QUOTES) ?>
+                <span class="name">（<?php echo htmlspecialchars($post['name'], ENT_QUOTES) ?>）</span>
+                [ <a href="index.php?res=<?php echo htmlspecialchars($post['id'], ENT_QUOTES); ?>">Re</a> ]
+            </p>
             <p class="day"><?php echo htmlspecialchars($post['created'], ENT_QUOTES) ?></p>
         </div>
 <?php
-endforeach
+endforeach;
 ?>
     </div>
 </div>
